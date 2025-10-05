a86bce23194ad5e566b6a9da1bdab89f2f54d7cb
Felix Deng <dengjianhao22@gmail.com>
2025-10-05 02:48:24 -0400

    modified page structure

--- src/app/course/new_course/page.tsx
+++ src/app/course/new_course/page.tsx
@@ -24,8 +24,7 @@ import {
   IconLayoutSidebarLeftExpand,
   IconUpload,
 } from '@tabler/icons-react';
-import TableOfTopics from '../components/tableOfTopics';
-
+import TableOfTopics from '../../components/tableOfTopics';
 
 const navList = [
   {
@@ -33,71 +32,72 @@ const navList = [
     label: 'Home',
   },
 ];
-const topicJson ={
-    "topics": {
-      "entries": [
-        {
-          "level": 1,
-          "page": 1,
-          "time": 42,
-          "title": "Chapter 1: Europe in 1914"
-        },
-        {
-          "level": 1,
-          "page": 15,
-          "time": 36,
-          "title": "Chapter 2: The Coming of War"
-        },
-        {
-          "level": 1,
-          "page": 27,
-          "time": 30,
-          "title": "Chapter 3: 1914: The Opening Campaigns"
-        },
-        {
-          "level": 1,
-          "page": 37,
-          "time": 57,
-          "title": "Chapter 4: 1915: The War Continues"
-        },
-        {
-          "level": 1,
-          "page": 56,
-          "time": 36,
-          "title": "Chapter 5: 1916: The War of Attrition"
-        },
-        {
-          "level": 1,
-          "page": 68,
-          "time": 39,
-          "title": "Chapter 6: The United States Enters the War"
-        },
-        {
-          "level": 1,
-          "page": 81,
-          "time": 42,
-          "title": "Chapter 7: 1917: The Year of Crisis"
-        },
-        {
-          "level": 1,
-          "page": 95,
-          "time": 54,
-          "title": "Chapter 8: 1918: The Year of Decision"
-        },
-        {
-          "level": 1,
-          "page": 113,
-          "time": 33,
-          "title": "Chapter 9: The Settlement"
-        }
-      ],
-      "total_entries": 9
-    }
-  }
+const topicJson = {
+  topics: {
+    entries: [
+      {
+        level: 1,
+        page: 1,
+        time: 42,
+        title: 'Chapter 1: Europe in 1914',
+      },
+      {
+        level: 1,
+        page: 15,
+        time: 36,
+        title: 'Chapter 2: The Coming of War',
+      },
+      {
+        level: 1,
+        page: 27,
+        time: 30,
+        title: 'Chapter 3: 1914: The Opening Campaigns',
+      },
+      {
+        level: 1,
+        page: 37,
+        time: 57,
+        title: 'Chapter 4: 1915: The War Continues',
+      },
+      {
+        level: 1,
+        page: 56,
+        time: 36,
+        title: 'Chapter 5: 1916: The War of Attrition',
+      },
+      {
+        level: 1,
+        page: 68,
+        time: 39,
+        title: 'Chapter 6: The United States Enters the War',
+      },
+      {
+        level: 1,
+        page: 81,
+        time: 42,
+        title: 'Chapter 7: 1917: The Year of Crisis',
+      },
+      {
+        level: 1,
+        page: 95,
+        time: 54,
+        title: 'Chapter 8: 1918: The Year of Decision',
+      },
+      {
+        level: 1,
+        page: 113,
+        time: 33,
+        title: 'Chapter 9: The Settlement',
+      },
+    ],
+    total_entries: 9,
+  },
+};
 export default function Home() {
   const { user, isLoading } = useUser();
   const [sideBarStatus, setSideBarStatus] = useState<boolean>(false);
   const [isFileProcessed, setIsFileProcessed] = useState<boolean>(false);
+  const [newCourseStep, setNewCourseStep] = useState<0 | 1 | 2 | 3>(0);
 
   const [file, setFile] = useState<File | null>(null);
 
@@ -200,9 +200,7 @@ export default function Home() {
       {/* Main Content */}
       <Center style={{ flex: 1, padding: 32 }}>
         {/* Add your main content here */}
-        {/* {isFileProcessed ? ( */}
-        {/* <TableOfTopics topicJson={topicJson}/> */}
-        {/* ) : (
+        {newCourseStep == 0 && (
           <Center
             style={{
               width: 'fit-content',
@@ -255,12 +253,18 @@ export default function Home() {
             <Button
               fullWidth
               disabled={!file}
-              onClick={() => setIsFileProcessed((o) => !o)}
+              onClick={() => {
+                setIsFileProcessed((o) => !o);
+                setNewCourseStep(1);
+              }}
             >
               Start
             </Button>
           </Center>
-        )*/}
+        )}
+        {newCourseStep == 1 && (
+          <TableOfTopics topicJson={topicJson} setNewCourseStep={setNewCourseStep} />
+        )}
       </Center>
     </Box>
   );
